e7714036019267eab809e4aaabaae69d69a88961
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-10-08 18:42:48 +0800

    Add project structure

---
+++ src/TirsvadWeb.Wasm.ProjectDocs.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TirsvadWeb.Wasm.ProjectDocs.WebUI.Data;
+
+namespace TirsvadWeb.Wasm.ProjectDocs.Infrastructure.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
